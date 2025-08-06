8563b6b19c47e941ba9642181f6cfb85af7fe351
Matviy4ik <matvejbajdik1@gmail.com>
2025-08-06 21:49:00 +0300

    fix

--- bibliogame/bibliogames/urls.py
+++ bibliogame/bibliogames/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 <<<<<<< HEAD
+<<<<<<< HEAD
 from .views.views import create_game, add_favorite_game, delete_favorite_game, game_detail
 from .views.moderation import moderation_list, moderate_game, delete_game, moderate_game_detail
 from bibliogames.views.game import index
 =======
+=======
+from .views.game import index
+>>>>>>> 2c0e963 (urls)
 from .views.views import create_game, delete_game, add_favorite_game, delete_favorite_game
 from .views.review import add_review, delete_review
 >>>>>>> 096ca5e (Reviews)
@@ -11,8 +15,13 @@ from .views.review import add_review, delete_review
 app_name = "bibliogames"
 
 urlpatterns = [
+<<<<<<< HEAD
     path('', index, name="index"),
     path('game/create/', create_game, name='create_game'),
+=======
+    path('', index, name='index'),
+    path('game/add/', create_game, name='create_game'),
+>>>>>>> 2c0e963 (urls)
     path('game/delete/<int:game_id>/', delete_game, name='delete_game'),
     path('favorite_game/add/<int:game_id>/', add_favorite_game, name='add_favorite_game'),
     path('favorite_game/delete/<int:game_id>/', delete_favorite_game, name='delete_favorite_game'),
